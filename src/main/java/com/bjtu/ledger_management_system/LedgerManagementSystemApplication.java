6ba30a83702037ca866ec893772d37dc8f0eb5d6
BJTU18301126 <1003964217@qq.com>
2021-04-09 19:17:16 +0800

    Initial commit

---
+++ src/main/java/com/bjtu/ledger_management_system/LedgerManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bjtu.ledger_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjtu/ledger_management_system/LedgerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjtu.ledger_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
