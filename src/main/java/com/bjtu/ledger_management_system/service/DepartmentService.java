cca6ddde7b374a37ae466dd3a3a423e511438c6e
MakerHu <1003964217@qq.com>
2021-04-22 23:23:31 +0800

    胡江浩 实现了部门管理

--- src/main/java/com/bjtu/ledger_management_system/service/DepartmentService.java
+++ src/main/java/com/bjtu/ledger_management_system/service/DepartmentService.java
@@ -1,4 +1,10 @@
 package com.bjtu.ledger_management_system.service;
 
+import com.bjtu.ledger_management_system.entity.Department;
+
 public interface DepartmentService {
+    void createDepartment(String superDid, Department newDepartment);
+    void modifyDepartment(Department department);
+    Department findByDname(String dname);
+    Department findByDid(String did);
 }
--- src/main/java/com/bjtu/ledger_management_system/serviceImpl/DepartmentServiceImpl.java
+++ src/main/java/com/bjtu/ledger_management_system/serviceImpl/DepartmentServiceImpl.java
@@ -1,15 +1,46 @@
 package com.bjtu.ledger_management_system.serviceImpl;
 
 import com.bjtu.ledger_management_system.dao.DepartmentDao;
+import com.bjtu.ledger_management_system.entity.Department;
 import com.bjtu.ledger_management_system.service.DepartmentService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
     @Resource
     private DepartmentDao departmentDao;
+
+    /**
+     * 在数据库中创建部门
+     * @param superDid
+     * @param newDepartment
+     */
+    @Override
+    public void createDepartment(String superDid, Department newDepartment) {
+        List<Department> departmentList = departmentDao.findByDidStartingWith(superDid);
+        int departmentCount = departmentList.size();
+        String newDid = superDid + "." + (departmentCount);
+        newDepartment.setDid(newDid);
+        departmentDao.save(newDepartment);
+    }
+
+    @Override
+    public void modifyDepartment(Department department) {
+        departmentDao.save(department);
+    }
+
+    @Override
+    public Department findByDname(String dname) {
+        return  departmentDao.findByDname(dname);
+    }
+
+    @Override
+    public Department findByDid(String did) {
+        return departmentDao.findById(did).orElse(null);
+    }
 
 
 }
