4eb4e0f859a6c51a1f258a2d06d8d222d93aecce
Chen_Fuxin <1183458938@qq.com>
2021-04-23 20:05:59 +0800

    陈付鑫完成了权限管理的部分修改333

---
+++ src/main/java/com/bjtu/ledger_management_system/serviceImpl/DTO/createRoleInDePartDTO.java
@@ -0,0 +1,13 @@
+package com.bjtu.ledger_management_system.serviceImpl.DTO;
+
+import com.bjtu.ledger_management_system.entity.Right;
+import com.bjtu.ledger_management_system.entity.Role;
+
+import java.util.List;
+
+public class createRoleInDePartDTO {
+    public Role role;
+    public List<Right> rightList;
+
+
+}
