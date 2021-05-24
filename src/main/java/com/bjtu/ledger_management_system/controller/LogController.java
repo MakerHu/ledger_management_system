4ac37cd6462c0074631843a774b518d0afbd7717
Chen_Fuxin <1183458938@qq.com>
2021-05-24 16:42:28 +0800

    陈付鑫修改部分参数

--- src/main/java/com/bjtu/ledger_management_system/controller/LogController.java
+++ src/main/java/com/bjtu/ledger_management_system/controller/LogController.java
@@ -5,10 +5,7 @@ import com.bjtu.ledger_management_system.common.Result;
 import com.bjtu.ledger_management_system.entity.Log;
 import com.bjtu.ledger_management_system.service.LogService;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -20,7 +17,7 @@ public class LogController {
 
     //获取所有日志
     @GetMapping
-    public Result<Page<Log>> getAllLog(Integer pageNum, Integer pageSize){
+    public Result<Page<Log>> getAllLog(@RequestParam Integer pageNum,@RequestParam Integer pageSize){
         Long uid=new Long("1");
         String content="查看了所有日志";
         logService.addLog(uid,content);
@@ -29,7 +26,7 @@ public class LogController {
 
     //模糊搜索
     @PostMapping
-    public Result<Page<Log>> getSpecificLogs(String content,Integer pageNum, Integer pageSize){
+    public Result<Page<Log>> getSpecificLogs(@RequestParam String content,@RequestParam Integer pageNum,@RequestParam Integer pageSize){
         Long uid=new Long("1");
         String contents="以字段“"+content+"”模糊查找了日志";
         logService.addLog(uid,contents);
